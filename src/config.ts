04fbc6a556fc8d7605d7ee5add4e709c37a258a9
Dan Isaacs <dan@danisaacs.net>
2024-05-15 22:05:56 -0600

    Fix lang, reenable analytics

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,10 @@ export const SITE: Site = {
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
-export const LOCALE = ["en-US"]; // set to [] to use the environment default
+export const LOCALE = {
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+} as const;
 
 export const LOGO_IMAGE = {
   enable: false,
